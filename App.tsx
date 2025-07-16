17727fc30848a3956e2d7b8e2e3f89a87cfd2dd5
277pawan <bpawan277@gmail.com>
2025-07-16 21:55:08 +0530

    docs: learning some new element of native

--- App.tsx
+++ App.tsx
@@ -9,11 +9,20 @@ import {
   Alert,
   SafeAreaView,
   Pressable,
+  useColorScheme,
+  View,
 } from 'react-native';
 const App: React.FC = () => {
+  const theme = useColorScheme();
+  const isDarkMode = theme === 'dark';
+  const backgroundColor = isDarkMode ? 'black' : 'white';
+  const textColor = isDarkMode ? 'white' : 'black';
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Welcome to AnyaAI!</Text>;{/* <Image */}
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: backgroundColor }]}
+    >
+      <Text style={{ color: textColor }}>Welcome to AnyaAI!</Text>;
+      {/* <Image */}
       {/*   source={{ */}
       {/*     uri: 'https://images.pexels.com/photos/28949655/pexels-photo-28949655.jpeg', */}
       {/*   }} */}
@@ -26,6 +35,12 @@ const App: React.FC = () => {
       >
         <Text>Button</Text>
       </TouchableOpacity>
+      <View>
+        <Text style={{ color: textColor }}>
+          {' '}
+          This is just for testing purpose.
+        </Text>
+      </View>
       <TouchableHighlight style={{ backgroundColor: 'green' }}>
         <Text>Button</Text>
       </TouchableHighlight>
